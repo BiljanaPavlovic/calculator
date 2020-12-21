45f7a75fa44c7694b34f92c5bd44d7dd9dcbea49
Biljana <zabiljanupavlovic@gmail.com>
2020-12-21 16:29:27 +0100

    work in progress

--- index.js
+++ index.js
@@ -22,103 +22,147 @@ let output = document.createElement('p')
 
 function broj() {
 
-
+    let niz = []
+    let i
+    let x
+    let y
     jedan.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent += 1
-        console.log(1)
+        i = output.textContent += '1'
+        niz.push(i)
+        console.log(niz)
+        //        let a = String(output)
+        //  console.log(typeof (a))
     }
     )
     dva.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent += 2
+        i = output.textContent += '2'
         console.log(2)
+        niz.push(i)
+        console.log(niz)
     }
     )
     tri.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent += 3
+        i = output.textContent += '3'
         console.log(3)
+        niz.push(i)
+        console.log(niz)
     }
     )
     cetiri.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent += 4
+        i = output.textContent += '4'
         console.log(4)
+        niz.push(i)
+        console.log(niz)
     }
     )
     pet.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent += 5
+        i = output.textContent += '5'
         console.log(5)
+        niz.push(i)
+        console.log(niz)
     }
     )
     sest.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent += 6
+        i = output.textContent += '6'
         console.log(6)
+        niz.push(i)
+        console.log(niz)
     }
     )
     sedam.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent += 7
+        i = output.textContent += '7'
         console.log(7)
+        niz.push(i)
+        console.log(niz)
     }
     )
     osam.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent += 8
+        i = output.textContent += '8'
         console.log(8)
+        niz.push(i)
+        console.log(niz)
     }
     )
     devet.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent += 9
+        i = output.textContent += '9'
         console.log(9)
+        niz.push(i)
+        console.log(niz)
     }
     )
     nula.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent += 0
-        console.log(0)
+        i = output.textContent += '0'
+        console.log(typeof (output))
+        niz.push(i)
+        console.log(niz)
     }
+
     )
-}
-//operatori
-function operatori() {
+    //}
+    //operatori
+    //function operatori(){
 
 
     plus.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent = '+'
+        i = output.textContent = '+'
         console.log('+')
+        niz.push(i)
+        console.log(niz)
     }
     )
     minus.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent = '-'
+        i = output.textContent = '-'
         console.log(0)
+        niz.push(i)
+        console.log(niz)
     }
     )
     podeljeno.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent = '/'
+        i = output.textContent = '/'
         console.log(0)
+        niz.push(i)
+        console.log(niz)
     }
     )
     puta.addEventListener('click', () => {
         input.appendChild(output)
-        output.textContent = '*'
+        i = output.textContent = '*'
         console.log(0)
+        niz.push(i)
+        console.log(niz)
+        x = i.join("")
+        console.log(x)
+    }
+    )
+    // let a = String(output)
+    // console.log(typeof (a))
+
+    jednako.addEventListener('click', () => {
+        input.appendChild(output)
+        y = eval(x)
+        console.log(x)
+        output.textContent = y
+        // let a = eval(output)
+        //  output.textContent = String(a)
+        console.log(y)
     }
     )
 }
-jednako.addEventListener('click', () => {
-    input.appendChild(output)
-    output.textContent = eval(broj() + operatori() + broj())
-    console.log(output)
-}
-)
 
 broj()
-operatori()
+//operatori()
+
+
